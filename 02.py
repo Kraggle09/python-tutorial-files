7eef0df36f01c7e361f30d820b477d087d2a01d6
Kraggle09 <redstoneweeklypodcast@gmail.com>
2025-09-23 14:00:36 -0500

    Added code for lesson 02

---
+++ 02.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print("How are you doing?")
+
+
+a = "Hello World"
+print(a)
+
+
+money = 438.29
+score = 87
+print("How much money do I have?")
+print(money)
+print("What percentage score did I get?")
+print(score)
+print("Am I happy?")
+print("Yes")
+
+
+# This sets a variable
+a = "Hello World"
+
+
+a = "Hello World" # This sets a variable
