13c8722063e69a15b526efd36c760ccc6f18ade4
Kraggle09 <redstoneweeklypodcast@gmail.com>
2025-09-24 11:13:25 -0500

    Added code for lesson 03

---
+++ 03.py
@@ -0,0 +1,43 @@
+print("hello".capitalize()) # Output: Hello
+print("hello".upper()) # Output: HELLO
+print("HELLO".lower()) # Output: hello
+
+myString = "hello"
+print(myString.capitalize()) # Output: Hello
+
+myString = "HeLlO"
+print(myString.upper()) # Output: HELLO
+print(myString.lower()) # Output: hello
+
+print(5 + 5) # Output: 10
+print(10 - 5) # Output: 5
+print(5 * 5) # Output: 25
+print(25 / 5) # Output: 5
+
+a = 5
+b = 10
+print(a + b) # Output: 15
+
+a = 5
+a += 10 # Adds the existing value (5) to the new value (10)
+print(a) # Output: 15
+
+print(3.2 + 4.6) # Output: 7.8
+print(8.2 - 5.3) # Output: 2.8999999999999995
+print(1.4 * 3.2) # Output: 4.4799999999999995
+print(2.2 / 1.2) # Output: 1.8333333333333335
+
+print("Hello " + "World") # Output: Hello World
+
+myInteger = 5
+myString = str(myInteger)
+print(myString) # Output: 5 (but it's a string instead of an integer)
+
+a = 5
+b = "10"
+c = a + int(b) # Converts the string "10" to a plain integer
+print(c) # Output: 15
+
+print(int(25 / 5)) # Output: 5
+
+print("My favorite number is: " + str(5)) # Output: My favorite number is: 5
