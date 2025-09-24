b82a9d05ee533f971fb5aa8ff4cf2eff1932c7c1
Kraggle09 <redstoneweeklypodcast@gmail.com>
2025-09-24 17:02:41 -0500

    Added code for lesson 04

---
+++ 04.py
@@ -0,0 +1,21 @@
+print("User inputted: " + input())
+
+print("User's name: " + input("What's your name? "))
+
+name = input("What's your name? ")
+print("Your name is " + name)
+print("So, " + name + ", how are you?")
+
+
+name = "Kraggle09"
+age = 16
+mood = "happy"
+print("Your name is " + name + ". You are " + str(age) + " years old, and are " + mood + ".")
+
+name = "Kraggle09"
+print(f"Your name is {name}")
+
+name = "Kraggle09"
+age = 16
+mood = "happy"
+print(f"Your name is {name}. You are {age} years old, and are {mood}.")
